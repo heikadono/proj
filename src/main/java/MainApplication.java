186dcc41208ffebe954c67cde4005500faa487c0
Tsepenkov Nikita <heikadono@yandex.ru>
2025-02-20 01:55:50 +0500

    Улучшим логику main

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -1,6 +1,6 @@
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+        System.out.println("Hello Worl");
+        System.out.println("Hello Wold");
     }
 }
